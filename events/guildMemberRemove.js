df74ae94638cdaa9297353b12e78ecaef4b229ca
Norethion <57538585+Norethion@users.noreply.github.com>
2020-02-29 22:35:52 +0300

    Add files via upload

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,12 +1,12 @@
 module.exports = member => {
-  console.log(`NAME:${member} | ID:${member.id} sunucudan ayrıldı.`)
-  
-  const guild = member.guild
+  console.log(`NAME:${member} | ID:${member.id} sunucudan ayrıldı.`);
+
+  const guild = member.guild;
   //panel
-  const kanalcık = db.fetch(`botPanel_${member.guild.id}`)
+  const kanalcık = db.fetch(`botPanel_${member.guild.id}`);
   if (kanalcık) {
-    const kanal = guild.channels.find('id', kanalcık)
-    if (!kanal) return db.delete(`botPanel_${guild.id}`)
-    kanal.setName(`Sunucudaki üye sayısı : ${guild.memberCount}`)
+    const kanal = guild.channels.find("id", kanalcık);
+    if (!kanal) return db.delete(`botPanel_${guild.id}`);
+    kanal.setName(`Sunucudaki üye sayısı : ${guild.memberCount}`);
   }
 };
--- events/message.js
+++ events/message.js
@@ -1,4 +1,7 @@
-const fs = require('fs');
+const Discord = require("discord.js");
+const fs = require("fs");
+const db = require("quick.db");
+const ayarlar = require("../ayarlar.json");
 let talkedRecently = new Set();
 
 module.exports = message => {
@@ -18,29 +21,100 @@ module.exports = message => {
   setTimeout(() => {
     talkedRecently.delete(message.author.id);
   }, 2500);
-  let client = message.client;
+
+  let bot = message.client;
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
-  let command = message.content.split(' ')[0].slice(prefix.length);
-  let params = message.content.split(' ').slice(1);
-  let perms = client.elevation(message);
-  let cmd;
+  let command = message.content.split(" ")[0].slice(prefix.length);
+  let params = message.content.split(" ").slice(1);
+  let perms = bot.elevation(message);
 
-  if (!client.commands.has(command)) {
-    if (client.aliases.has(command)) {
-      return false;
+  if (!bot.commands.has(command)) {
+    if (bot.aliases.has(command)) {
+      //return false;
     } else {
-      message.channel.send(`Komutlarımda \`\`${command}\`\` adında bir komut bulamadım! Komut listesine bakmak için: \`\`${prefix}yardım\`\``)
+      message.channel.send(
+        `Komutlarımda \`\`${command}\`\` adında bir komut bulamadım! Komut listesine bakmak için: \`\`${prefix}yardım\`\``
+      );
     }
   }
+  //
+  const cmd =
+    bot.commands.get(command) || bot.commands.get(bot.aliases.get(command));
+  if (cmd) {
+    if (cmd.conf.enabled === false) {
+      if (
+        !ayarlar.sahip.includes(message.author.id) &&
+        !ayarlar.sahip.includes(message.author.id)
+      ) {
+        const embed = new Discord.RichEmbed()
+          .setDescription(
+            `:x: **${cmd.help.name}** isimli komut şuanda geçici olarak kullanıma kapalıdır!`
+          )
+          .setColor("RED");
+        message.channel.send({ embed });
+        return;
+      }
+    }
 
-  if (client.commands.has(command)) {
-    cmd = client.commands.get(command);
-  } else if (client.aliases.has(command)) {
-    cmd = client.commands.get(client.aliases.get(command));
+    if (cmd.conf.permLevel === 1) {
+      if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+        const embed = new Discord.RichEmbed()
+          .setDescription(
+            `Bu komutu kullanabilmek için **Mesajları Yönet** iznine sahip olmalısın!`
+          )
+          .setColor("RED");
+        message.channel.send({ embed });
+        return;
+      }
+    }
+    if (cmd.conf.permLevel === 2) {
+      if (!message.member.hasPermission("KICK_MEMBERS")) {
+        const embed = new Discord.RichEmbed()
+          .setDescription(
+            `Bu komutu kullanabilmek için **Üyeleri At** iznine sahip olmalısın!`
+          )
+          .setColor("RED");
+        message.channel.send({ embed });
+        return;
+      }
+    }
+    if (cmd.conf.permLevel === 3) {
+      if (!message.member.hasPermission("BAN_MEMBERS")) {
+        const embed = new Discord.RichEmbed()
+          .setDescription(
+            `Bu komutu kullanabilmek için **Üyeleri Yasakla** iznine sahip olmalısın!`
+          )
+          .setColor("RED");
+        message.channel.send({ embed });
+        return;
+      }
+    }
+    if (cmd.conf.permLevel === 4) {
+      if (!message.member.hasPermission("ADMINISTRATOR")) {
+        const embed = new Discord.RichEmbed()
+          .setDescription(
+            `Bu komutu kullanabilmek için **Yönetici** iznine sahip olmalısın!`
+          )
+          .setColor("RED");
+        message.channel.send({ embed });
+        return;
+      }
+    }
+    if (cmd.conf.permLevel === 5) {
+      if (!ayarlar.sahip.includes(message.author.id)) {
+        const embed = new Discord.RichEmbed()
+          .setDescription(`Bu komutu sadece **Sahibim** kullanabilir!`)
+          .setColor("RED");
+        message.channel.send({ embed });
+        return;
+      }
+    }
   }
+  //
+
   if (cmd) {
     if (perms < cmd.conf.permLevel) return;
-    cmd.run(client, message, params, perms);
+    cmd.run(bot, message, params, perms);
   }
 };
